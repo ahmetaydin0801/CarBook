908f64eb87d289ee0cc4894ec9f17e60c15dcf4e
Ahmet Aydın <ahmet.aydin@pubinno.com>
2024-12-14 17:54:23 +0300

    define CarBookContext.cs and sql connection

--- Infrastructure/CarBook.Persistance/Context/CarBookContext.cs
+++ Infrastructure/CarBook.Persistance/Context/CarBookContext.cs
@@ -1,6 +1,31 @@
+using CarBook.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace CarBook.Persistance.Context;
 
-public class CarBookContext
+public class CarBookContext:DbContext
 {
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=CarBookDatabase;TrustServerCertificate=True;Trusted_Connection=False;User Id=sa;Password=SQLConnect1!");
+    }
+    public DbSet<About> Abouts { get; set; }
+    public DbSet<Banner> Banners { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<CarDescription> CarDescriptions { get; set; }
+    public DbSet<CarFeature> CarFeatures { get; set; }
+    public DbSet<CarPricing> CarPricings { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Feature> Features { get; set; }
+    public DbSet<FooterAddress> FooterAddresses { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Pricing> Pricings { get; set; }
+    public DbSet<Service> Services { get; set; }
+    public DbSet<SocialMedia> SocialMedias { get; set; }
+    public DbSet<Testimonial> Testimonials { get; set; }
+
+
+
 }

f8c8def8358f45718fdbcaeee93afaf0ff123708
Ahmet Aydın <ahmet.aydin@pubinno.com>
2024-12-21 16:02:51 +0300

    create Banner CQRS setup

--- Core/CarBook.Application/Features/CQRS/Results/AboutResults/GetAboutQueryResult.cs
+++ Core/CarBook.Application/Features/CQRS/Results/AboutResults/GetAboutQueryResult.cs
@@ -5,5 +5,5 @@ public class GetAboutQueryResult
     public int AboutId { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public string ImageUrl { get; set; }
+    public string ImageUrl { get; set; } 
 }
